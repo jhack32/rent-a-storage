ab25ae16e54fcc8febbe9628301e6bc33737d719
Jack Huang <Jack7100@gmail.com>
2015-12-19 18:19:09 -0500

    User login page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id
+      redirect_to '/'
+    else
+      redirect_to '/users/new'
+    end
+  end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
